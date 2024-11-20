c573fd24690e7222aa5b617ed952cd9f77d2a5e0
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-11-20 16:05:12 -0300

    feat: better print logs

--- app/handlers/logs.go
+++ app/handlers/logs.go
@@ -15,9 +15,30 @@ func Logs() {
 	errors.Check(err)
 
 	repository := repositories.GetRepository(root)
-	logs := repository.GetLogs()
+	log := repository.GetLogs()
 
-	for _, log := range logs {
-		fmt.Fprintf(os.Stdout, "\033[33m %s \033[0m %s \033[32m %s\n", log.Checkpoint.Id, log.Checkpoint.Message, log.Checkpoint.CreatedAt.Format(DATE_LAYOUT))
+	for _, saveLog := range log.History {
+		fmt.Fprintf(os.Stdout, "\033[33m %s ", saveLog.Checkpoint.Id)
+
+		if len(saveLog.Refs) > 0 {
+			fmt.Fprint(os.Stdout, "\033[34m(")
+
+			for idx, ref := range saveLog.Refs {
+				if ref == log.Head {
+					fmt.Fprintf(os.Stdout, "HEAD -> %s", ref)
+				} else {
+					fmt.Fprintf(os.Stdout, "%s", ref)
+				}
+
+				if idx < len(saveLog.Refs)-1 {
+					fmt.Fprint(os.Stdout, ", ")
+				}
+			}
+
+			fmt.Fprint(os.Stdout, ")")
+		}
+
+		fmt.Fprintf(os.Stdout, "\033[0m %s ", saveLog.Checkpoint.Message)
+		fmt.Fprintf(os.Stdout, "\033[32m %s\n", saveLog.Checkpoint.CreatedAt.Format(DATE_LAYOUT))
 	}
 }
